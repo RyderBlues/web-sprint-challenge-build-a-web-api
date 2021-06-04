5924fdbb08c619babc7d98e56f15d2609474dd9b
Ryder <ryderblue2@gmail.com>
2021-06-04 12:10:05 -0700

    finished mvp

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,98 @@
 // Write your "actions" router here!
+const express = require('express');
+const router = express.Router();
+const Actions = require('./actions-model');
+
+router.get('/', (req, res) => {
+    Actions.get()
+        .then(action => {
+            if (!action) {
+                res.status(404).json([]);
+            }
+            else {
+                res.status(200).json(action);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting actions',
+                error: err.message});
+        });
+})
+
+router.get('/:id', (req, res) => {
+    Actions.get(req.params.id)
+        .then(action => {
+            if(!action) {
+                res.status(404).json({
+                    message: 'action not found'
+                });
+            }
+            else {
+                res.status(200).json(action);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error Finding action',
+                error: err.message
+            });
+        });
+});
+
+router.post('/', (req, res) => {
+    Actions.insert(req.body)
+        .then(action => {
+            if (!req.body.project_id || !req.body.description || !req.body.notes) {
+                res.status(400).json({
+                    message: 'Actions must have a project id, description, and notes.'
+                });
+            }
+            else {
+                res.status(201).json(action);
+            }
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'Error posting new Action',
+                error: err.message
+            });
+        });
+});
+
+router.put('/:id', (req, res) => {
+    Actions.update(req.params.id, req.body)
+        .then(updated => {
+            if (!updated) {
+                res.status(404).json({
+                    message: 'no action found'
+                });
+            }
+            else {
+                res.status(200).json(updated);
+            }
+        })
+        .catch(() => {
+            res.status(400).json({
+                message: 'error updating action'
+            });
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    Actions.remove(req.params.id)
+        .then(deleted => {
+            if (!req.body.id) {
+                res.status(404).json({message: 'no project found'})
+            }
+            else {
+                res.status(200).json(deleted);
+            }
+        })
+        .catch(err => {
+            res.status(404).json({
+                error: err.message});
+        });
+});
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,120 @@
 // Write your "projects" router here!
+const express = require('express');
+const router = express.Router();
+const Projects = require('./projects-model');
+
+// [GET] all projects
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(project => {
+            if (!project) {
+                res.status(404).json([]);
+            }
+            else {
+                res.status(200).json(project);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting projects',
+                error: err.message});
+        });
+});
+// [GET] proj by id
+router.get('/:id', (req, res) => {
+    Projects.get(req.params.id)
+        .then(project => {
+            if(!project) {
+                res.status(404).json({
+                    message: 'Project not found'
+                });
+            }
+            else {
+                res.status(200).json(project);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error Finding Project',
+                error: err.message
+            });
+        });
+});
+
+// [POST] proj
+router.post('/', (req, res) => {
+    Projects.insert(req.body)
+        .then(newProj => {
+            if (!req.body.name || !req.body.description) {
+                res.status(400).json({
+                    message: 'Project must have name and description'
+                });
+            }
+            else {
+                res.status(201).json(newProj);
+            }
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'Error posting new Project',
+                error: err.message
+            });
+        });
+});
+
+// [PUT] update by proj id
+router.put('/:id', (req, res) => {
+    Projects.update(req.params.id, req.body)
+        .then(updated => {
+            if (!updated) {
+                res.status(404).json({
+                    message: 'no project found'
+                });
+            }
+            else {
+                res.status(200).json(updated);
+            }
+        })
+        .catch(() => {
+            res.status(400).json({
+                message: 'error updating project'
+            });
+        });
+});
+
+// [DELETE] by proj id
+router.delete('/:id', (req, res) => {
+    Projects.remove(req.params.id)
+        .then(deleted => {
+            if (!req.body.id) {
+                res.status(404).json({message: 'no project found'})
+            }
+            else {
+                res.status(200).json(deleted);
+            }
+        })
+        .catch(err => {
+            res.status(404).json({
+                error: err.message});
+        });
+});
+
+// [GET] actions by project id
+router.get('/:id/actions', (req, res) => {
+    Projects.getProjectActions(req.params.id)
+        .then(resp => {
+            if (!resp) {
+                res.status(404).json({message: 'no project found'});
+            }
+            else {
+                res.status(200).json(resp);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err.message
+            });
+        });
+});
+
+module.exports = router;
